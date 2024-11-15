c66124777f903a336091f1d8908f5bb2c8a116d9
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-11-16 01:39:54 +0200

    integrate strategy and state patterns - Add strategy and state properties to Character class - Implement state and strategy changing methods - Add default state and strategy initialization - Preserve existing character attributes

--- GameDesignPatterns/Models/Characters/Character.cs
+++ GameDesignPatterns/Models/Characters/Character.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameDesignPatterns.Patterns;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,26 +7,54 @@ using System.Threading.Tasks;
 
 namespace GameDesignPatterns.Models
 {
-    // Factory Method Pattern - Character Creation
-
-    // Abstract Character class
     public abstract class Character
     {
-        public string name { get; }
-        public int health { get; }
-        public int mana { get; }
-        public int strength { get; }
-        public int agility { get; }
+        // Properties from your implementation
+        public string Name { get; }
+        public int Health { get; protected set; }
+        public int Mana { get; protected set; }
+        public int Strength { get; protected set; }
+        public int Agility { get; protected set; }
+
+        // Strategy and State pattern properties
+        public IActionStrategy CurrentActionStrategy { get; set; }
+        public ICharacterState CurrentState { get; set; }
 
         protected Character(string name, int health, int mana, int strength, int agility)
         {
-            this.name = name;
-            this.health = health;
-            this.mana = mana;
-            this.strength = strength;
-            this.agility = agility;
+            Name = name;
+            Health = health;
+            Mana = mana;
+            Strength = strength;
+            Agility = agility;
+
+            // Initialize with default state and strategy
+            CurrentState = new IdleState();
+            InitializeDefaultStrategy();
         }
 
+        // Method to be implemented by derived classes to set their default strategy
+        protected abstract void InitializeDefaultStrategy();
+
+        // Abstract method from your implementation
         public abstract string UseAbility();
+
+        // Strategy pattern methods
+        public void ChangeActionStrategy(IActionStrategy newStrategy)
+        {
+            CurrentActionStrategy = newStrategy;
+        }
+
+        // State pattern methods
+        public void ChangeState(ICharacterState newState)
+        {
+            CurrentState = newState;
+            CurrentState.HandleState(this);
+        }
+
+        public void PerformAction()
+        {
+            CurrentState.HandleState(this);
+        }
     }
 }
