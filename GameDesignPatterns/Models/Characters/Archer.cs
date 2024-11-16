8b1a98d189a1ba3550f79f76cc5e51e76474fc0e
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-11-16 21:11:26 +0200

    Add default strategies for Warrior, Mage and Archer; implement MeleeAction MagicAction and RangedAction.

--- GameDesignPatterns/Models/Characters/Archer.cs
+++ GameDesignPatterns/Models/Characters/Archer.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using GameDesignPatterns.Patterns;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameDesignPatterns.Models
+namespace GameDesignPatterns.Models.Characters
 {
     public class Archer : Character
     {
-        public Archer(string name) : base(name, 70, 40, 7, 15) { }
-        public override string UseAbility() => $"{name} shoots Precise Arrow!";
+        public Archer(string name) : base(name, 70, 40, 7, 15) 
+        {
+            InitializeDefaultStrategy();
+        }
+
+        protected override void InitializeDefaultStrategy()
+        {
+            CurrentActionStrategy = new RangedAction();
+        }
+        public override string UseAbility() => $"{Name} shoots Precise Arrow!";
     }
 }
--- GameDesignPatterns/Models/Characters/Warrior.cs
+++ GameDesignPatterns/Models/Characters/Warrior.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameDesignPatterns.Patterns;
 
-namespace GameDesignPatterns.Models
+namespace GameDesignPatterns.Models.Characters
 {
     public class Warrior : Character
     {
-        public Warrior(string name) : base(name, 100, 20, 15, 8) { }
-        public override string UseAbility() => $"{name} uses Mighty Slash!";
+        public Warrior(string name) : base(name, 100, 20, 15, 8) 
+        {
+            InitializeDefaultStrategy();
+        }
+
+        protected override void InitializeDefaultStrategy()
+        {
+            CurrentActionStrategy = new MeleeAction();
+        }
+        public override string UseAbility() => $"{Name} uses Mighty Slash!";
     }
 }
