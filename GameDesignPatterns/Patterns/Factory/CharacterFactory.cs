0945944d5f24d329c6c8002f85dfe3f49b09fe38
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-11-16 21:21:34 +0200

    Update using directives

--- GameDesignPatterns/Patterns/Factory/CharacterFactory.cs
+++ GameDesignPatterns/Patterns/Factory/CharacterFactory.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GameDesignPatterns.Models;
+using GameDesignPatterns.Models.Characters;
 
 namespace GameDesignPatterns.Patterns.Factory
 {
