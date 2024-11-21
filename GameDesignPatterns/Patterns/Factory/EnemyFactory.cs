116add88c672b4addc4e290ebe9badcfdae1190d
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-11-21 12:56:06 +0200

    Add factory patterns (EnemyFactory, QuestFactory)

---
+++ GameDesignPatterns/Patterns/Factory/EnemyFactory.cs
@@ -0,0 +1,24 @@
+﻿using GameDesignPatterns.Enums;
+using GameDesignPatterns.Models.Enemies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDesignPatterns.Patterns.Factory
+{
+    public class EnemyFactory
+    {
+        public IEnemy CreateEnemy(string enemyType, EnemyRank rank)
+        {
+            return enemyType.ToLower() switch
+            {
+                "slime" => new Slime(rank),
+                "goblin" => new Goblin(rank),
+                "dragon" => new Dragon(rank),
+                _ => throw new ArgumentException("Invalid enemy type")
+            };
+        }
+    }
+}
---
+++ GameDesignPatterns/Patterns/Factory/QuestFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using GameDesignPatterns.Models.Quests;
+
+namespace GameDesignPatterns.Patterns.Factory
+{
+    public interface IQuestFactory
+    {
+        IQuest CreateMainQuest(string title, string description, int requiredKills);
+        IQuest CreateSideQuest(string title, string description, List<string> objectives);
+    }
+
+    public class QuestFactory : IQuestFactory
+    {
+        public IQuest CreateMainQuest(string title, string description, int requiredKills)
+        {
+            return new MainQuest(title, description, requiredKills);
+        }
+
+        public IQuest CreateSideQuest(string title, string description, List<string> objectives)
+        {
+            return new SideQuest(title, description, objectives);
+        }
+    }
+}
