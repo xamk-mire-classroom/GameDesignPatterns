2cd0033d07fa2f5e8be7b98268e05314f1c52c6b
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-12-13 23:15:06 +0200

    Add LocationType and NPCType Enums

---
+++ GameDesignPatterns/Enums/NPCType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDesignPatterns.Enums
+{
+    public enum NPCType
+    {
+        Villager,
+        Merchant,
+        Royalty
+    }
+}
