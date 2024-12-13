85c0b745eb72daa4c8c2847ef5850514d0c0a255
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-12-13 13:24:18 +0200

    Add command type enum

---
+++ GameDesignPatterns/Enums/CommandType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDesignPatterns.Enums
+{
+    public enum CommandType 
+    {
+        Attack,
+        Defend,
+        Heal,
+        Move,
+        Macro
+    }
+}
